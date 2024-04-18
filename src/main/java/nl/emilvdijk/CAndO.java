4fdba1bd6da4ccab868b9accad4bbb8f5002e40d
emilvdijk <emilvdijk@gmail.com>
2024-04-18 20:13:56 +0200

    initial commit

---
+++ src/main/java/nl/emilvdijk/CAndO.java
@@ -0,0 +1,11 @@
+package nl.emilvdijk;
+
+/** Hello world! */
+public class CAndO {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    double x = 0.2;
+    double y = 0.1;
+    System.out.print(x * y);
+  }
+}
---
+++ src/test/java/nl/emilvdijk/CAndOTest.java
@@ -0,0 +1,20 @@
+package nl.emilvdijk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CAndOTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
